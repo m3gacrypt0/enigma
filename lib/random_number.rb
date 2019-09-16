b63b4d22031d0a7f203af30754e8bb82dfeb88a1
Allison E. Moore <m3gacrypt0@gmail.com>
2019-09-16 17:46:55 -0600

    Add functionality for generate and get_digit methods in RandomNumber class

--- lib/random_number.rb
+++ lib/random_number.rb
@@ -3,5 +3,16 @@ class RandomNumber
 
   def initialize(length)
     @length = length
+    @source_digits = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+  end
+
+  def generate
+    output = ""
+    @length.times { output += get_digit}
+    output
+  end
+
+  def get_digit
+    @source_digits.sample.to_s
   end
 end
--- test/random_number_test.rb
+++ test/random_number_test.rb
@@ -1,11 +1,11 @@
-require 'minitest/autorun'
-require 'minitest/pride'
+require './test/test_helper'
 require './lib/random_number'
 
 class RandomNumberTest < Minitest::Test
 
   def setup
     @randomnumber = RandomNumber.new(5)
+    @randomnumber2 = RandomNumber.new(10)
   end
 
   def test_it_exists
@@ -18,9 +18,11 @@ class RandomNumberTest < Minitest::Test
 
   def test_method_generate
     assert_equal 5, @randomnumber.generate.length
+    assert_equal 10, @randomnumber2.generate.length
   end
 
   def test_method_get_digit
-    assert_includes [0, 1, 2, 3, 4, 5, 6, 7, 8, 9], @randomnumber.get_digit
+    assert_includes ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'], @randomnumber.get_digit
+    assert_equal 1, @randomnumber.get_digit.length
   end
 end
