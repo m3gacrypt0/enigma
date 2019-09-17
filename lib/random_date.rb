bff33e0e2e984ca0bb3c5677094639bf86f36457
Allison E. Moore <m3gacrypt0@gmail.com>
2019-09-16 20:00:50 -0600

    Update random_number_test for test_method_generate tests

--- lib/random_date.rb
+++ lib/random_date.rb
@@ -1,5 +1,7 @@
 class RandomDate
 
-
+def initialize(date_supplied)
+  @date_supplied  = date_supplied
+end
 
 end
--- test/random_date_test.rb
+++ test/random_date_test.rb
@@ -4,11 +4,17 @@ require './lib/random_date'
 class RandomDateTest < Minitest::Test
 
   def setup
-    @random_date = RandomDate.new
+    @random_date = RandomDate.new(false)
+    @random_date2 = RandomDate.new(true)
   end
 
   def test_it_exists
     assert_instance_of RandomDate, @random_date
+  end
+
+  def test_method_generate
+    assert_equal 6, RandomDate.new(false).length
+    assert_equal 6, RandomDate.new(true).length
   end
 
 end
