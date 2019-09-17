f8fc3edcd66c5f44701af9f9536c181b22278cb0
Allison E. Moore <m3gacrypt0@gmail.com>
2019-09-16 21:17:55 -0600

    Add files for encryptable_test.rb and empty Encryptable module

---
+++ lib/encryptable.rb
@@ -0,0 +1,3 @@
+module Encryptable
+
+end
---
+++ test/encryptable_test.rb
@@ -0,0 +1,11 @@
+require './test/test_helper'
+# require './lib/enigma'
+require './lib/encryptable'
+
+class EnigmaTest < Minitest::Test
+
+  def setup
+    
+  end
+
+end
