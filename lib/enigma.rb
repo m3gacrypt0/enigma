ff6b3a631754ae238a6a9bfec2859cf6690fa486
Allison E. Moore <m3gacrypt0@gmail.com>
2019-09-16 21:15:20 -0600

    Update Enigma class to include encrypt method

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -11,6 +11,17 @@ class Enigma
     @message  = message
     @key      = key
     @date     = date
+    @offsets  = { :a => get_char_offset('A', @key),
+                  :b => get_char_offset('B', @key),
+                  :c => get_char_offset('C', @key),
+                  :d => get_char_offset('D', @key)
+                }
+  end
+
+  def self.encrypt(message, key, date)
+    enigma = Enigma.new(message, key, date)
+
+
   end
 
 end
