fc1ff5d9347fa52b836ba7e76415d8d589bc48b7
Allison E. Moore <m3gacrypt0@gmail.com>
2019-09-16 21:39:28 -0600

    Update encryptable_test.rb with test for method get_keys

--- test/encryptable_test.rb
+++ test/encryptable_test.rb
@@ -5,7 +5,20 @@ require './lib/encryptable'
 class EnigmaTest < Minitest::Test
 
   def setup
-    
+    @enigma = Enigma.new("hello world", '02715', '040895')
+    @enigma2 = Enigma.new("Hello world")
+    @enigma3 = Enigma.new("Hello world", '04567')
+    # @enigma4 = Enigma.new("Hello world", nil, '160919')
+    @today = Date.today.strftime('%d%m%y')
+
+  end
+
+  def test_method_get_keys
+    expected =  {:a_key => '02',
+                 :b_key => '27',
+                 :c_key => '71',
+                 :d_key => '15'}
+    assert_equal expected, get_keys('02715')
   end
 
 end
